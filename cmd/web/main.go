4cdd2b3cbc677a8639acc1550289f27847848859
Dewald Hanekom <dewaldh@pepkorit.com>
2021-06-23 21:04:29 +0200

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/dhanekom/bookings/pkg/config"
+	"github.com/dhanekom/bookings/pkg/handlers"
+	"github.com/dhanekom/bookings/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("unable to create template cache")
+	}
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	app.UseCache = false
+	app.TemplateCache = tc
+	render.NewTemplates(&app)
+	handlers.NewRepo(&app)
+
+	log.Printf("Starting server on port %s\n", portNumber)
+	srv := http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ cmd/web/router.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/dhanekom/bookings/pkg/config"
+	"github.com/dhanekom/bookings/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(a *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
